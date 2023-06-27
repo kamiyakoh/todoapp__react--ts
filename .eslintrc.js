53c59b4dfd61b64a24962d5e0ec58a538b1420d1
kamiyakoh <nanayou791@gmail.com>
2023-06-27 10:15:05 +0900

    TypeScript用eslintパッケージを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,38 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'standard-with-typescript',
     'plugin:react/recommended',
-    'prettier'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
-  }
-}
+    // React17から不要になった import React from 'react'; に対応
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    // 不適切な依存配列の検知
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};

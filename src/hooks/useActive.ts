72b76c68583d25bd244724c6e679e79ead022403
kamiyakoh <nanayou791@gmail.com>
2023-07-12 05:23:42 +0900

    useActive.ts&useCustomForm.ts作成

---
+++ src/hooks/useActive.ts
@@ -0,0 +1,33 @@
+import { useRecoilState } from 'recoil';
+import { useCallback } from 'react';
+import type { TodoData } from '../types';
+import { activeState } from '../states/activeState';
+
+interface UseActive {
+  active: TodoData[];
+  setNewActive: (newActive: TodoData[]) => void;
+  delActive: (id: number) => void;
+}
+
+export const useActive = (): UseActive => {
+  const [active, setActive] = useRecoilState(activeState);
+  const setNewActive = useCallback(
+    (newActive: TodoData[]) => {
+      setActive(newActive);
+    },
+    [setActive]
+  );
+  const delActive = useCallback(
+    (id: number) => {
+      const filteredActive = active.filter((item) => item !== active[id]);
+      const fixedIdActive = filteredActive.map((item, index) => ({
+        ...item,
+        id: index,
+      }));
+      setNewActive(fixedIdActive);
+    },
+    [active, setNewActive]
+  );
+
+  return { active, setNewActive, delActive };
+};
---
+++ src/hooks/useCustomForm.ts
@@ -0,0 +1,172 @@
+import type { UseFormRegister, UseFormHandleSubmit, FieldArrayWithId } from 'react-hook-form';
+import type { TodoTask, TodoData } from '../types';
+import { useState, useEffect, useCallback } from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { useActive } from './useActive';
+import { toastSuccess, toastError } from '../utils/customToast';
+
+interface FormValues {
+  title: string;
+  tasks: Array<{ value: string }>;
+}
+interface UseCustomForm {
+  register: UseFormRegister<FormValues>;
+  handleSubmit: UseFormHandleSubmit<FormValues>;
+  fields: Array<FieldArrayWithId<FormValues, 'tasks', 'id'>>;
+  isError: boolean;
+  submitNew: (data: TodoData) => void;
+  isInline: boolean;
+  taskCount: number;
+  addTask: () => void;
+  reduceTask: (number: number) => void;
+  startComposition: () => void;
+  endComposition: () => void;
+  onKeydownTitle: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onKeydown: (e: React.KeyboardEvent<HTMLInputElement>, index: number) => void;
+}
+
+export const useCustomForm = (): UseCustomForm => {
+  const { active, setNewActive } = useActive();
+  // React Hook Form用宣言
+  const { register, handleSubmit, control, reset, setFocus, getValues } = useForm<FormValues>({
+    defaultValues: {
+      title: '',
+      tasks: [{ value: '' }],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'tasks',
+  });
+  // submitボタンを押した時
+  const [isError, setIsError] = useState(false);
+
+  const submitNew = useCallback(
+    (data: TodoData) => {
+      let isTask = false;
+      const dataTask: TodoTask[] = data.tasks;
+      const taskValues = dataTask
+        .map((item, index) => {
+          let task = null;
+          if (item.value !== '') {
+            task = {
+              taskNum: index,
+              value: item.value,
+              checked: false,
+            };
+            isTask = true;
+          }
+          return task;
+        })
+        .filter(Boolean) as TodoTask[];
+      if (isTask) {
+        setIsError(false);
+        const newBoard: TodoData = {
+          id: active.length ?? 0,
+          title: data.title,
+          tasks: taskValues,
+        };
+        const newActive: TodoData[] = [...active, newBoard];
+        setNewActive(newActive);
+        setFocus('title');
+        reset();
+        isTask = false;
+        toastSuccess();
+      } else {
+        setIsError(true);
+        toastError();
+        setFocus(`tasks.0.value`);
+      }
+    },
+    [active, reset, setFocus, setIsError, setNewActive]
+  );
+
+  // することのinput欄を増減
+  const [isInline, setIsInline] = useState(false);
+  const [taskCount, setTaskCount] = useState(0);
+  useEffect(() => {
+    if (taskCount > 0) {
+      setIsInline(true);
+    } else if (taskCount < 2) {
+      setIsInline(false);
+    }
+  }, [taskCount, setIsInline]);
+  const addTask = (): void => {
+    append({ value: '' });
+    setTaskCount(taskCount + 1);
+  };
+  const reduceTask = (number: number): void => {
+    remove(number);
+    setTaskCount(taskCount - 1);
+  };
+  // 漢字変換・予測変換（サジェスト）選択中か否かの判定
+  const [composing, setComposition] = useState(false);
+  const startComposition = useCallback(() => {
+    setComposition(true);
+  }, [setComposition]);
+  const endComposition = useCallback(() => {
+    setComposition(false);
+  }, [setComposition]);
+  // タイトル入力中にエンターですること入力欄にフォーカス
+  const onKeydownTitle = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      switch (e.key) {
+        // 変換中でない時に エンター で input を増やす
+        case 'Enter':
+          e.preventDefault();
+          if (composing) break;
+          setFocus('tasks.0.value');
+          break;
+        default:
+          break;
+      }
+    },
+    [composing, setFocus]
+  );
+  // input入力時にキーボード操作でinput欄を増減
+  const onKeydown = (e: React.KeyboardEvent<HTMLInputElement>, index: number): void => {
+    const value = getValues(`tasks.${index}.value`);
+    switch (e.key) {
+      // 変換中でない時に エンター で input を増やす
+      case 'Enter':
+        e.preventDefault();
+        if (composing) break;
+        addTask();
+        break;
+      // input が空欄時に バックスペース で input を減らす
+      case 'Backspace':
+        if (value === '') {
+          if (taskCount === 0) {
+            setFocus('title');
+          } else {
+            reduceTask(index);
+            if (index === 0) {
+              setFocus('title');
+            } else {
+              const prev: number = index - 1;
+              setFocus(`tasks.${prev}.value`);
+            }
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  return {
+    register,
+    handleSubmit,
+    fields,
+    isError,
+    submitNew,
+    isInline,
+    taskCount,
+    addTask,
+    reduceTask,
+    startComposition,
+    endComposition,
+    onKeydownTitle,
+    onKeydown,
+  };
+};

9bac474c2dabd2b4ab3f10c970c3ada9843d1f2a
kamiyakoh <nanayou791@gmail.com>
2024-02-29 01:23:50 +0900

    useActive.test.tsx, useComp.test.tsx修正

--- src/hooks/__test__/useActive.test.tsx
+++ src/hooks/__test__/useActive.test.tsx
@@ -8,97 +8,101 @@ interface Props {
   children: ReactNode;
 }
 
-const initializeState = ({ set }: MutableSnapshot): void => {
-  set(activeState, []);
-};
-const TestWrapper: FC<Props> = ({ children }) => <RecoilRoot initializeState={initializeState}>{children}</RecoilRoot>;
+describe('UseActive Test', () => {
+  const initializeState = ({ set }: MutableSnapshot): void => {
+    set(activeState, []);
+  };
+  const TestWrapper: FC<Props> = ({ children }) => (
+    <RecoilRoot initializeState={initializeState}>{children}</RecoilRoot>
+  );
 
-test('SetNewActive Test', () => {
-  const { result } = renderHook(() => useActive(), {
-    wrapper: TestWrapper,
+  test('SetNewActive Test', () => {
+    const { result } = renderHook(() => useActive(), {
+      wrapper: TestWrapper,
+    });
+
+    const newActive = [
+      {
+        id: 0,
+        title: 'Todo List1',
+        tasks: [
+          { taskNum: 0, value: 'Task1', checked: false },
+          { taskNum: 1, value: 'Task2', checked: false },
+        ],
+      },
+      {
+        id: 1,
+        title: '',
+        tasks: [
+          { taskNum: 0, value: 'TaskA', checked: false },
+          { taskNum: 1, value: 'TaskB', checked: false },
+        ],
+      },
+    ];
+
+    act(() => {
+      result.current.setNewActive(newActive);
+    });
+    expect(result.current.active).toEqual(newActive);
   });
 
-  const newActive = [
-    {
-      id: 0,
-      title: 'Todo List1',
-      tasks: [
-        { taskNum: 0, value: 'Task1', checked: false },
-        { taskNum: 1, value: 'Task2', checked: false },
-      ],
-    },
-    {
-      id: 1,
-      title: '',
-      tasks: [
-        { taskNum: 0, value: 'TaskA', checked: false },
-        { taskNum: 1, value: 'TaskB', checked: false },
-      ],
-    },
-  ];
+  test('DeleteActive Test', () => {
+    const { result } = renderHook(() => useActive(), {
+      wrapper: TestWrapper,
+    });
 
-  act(() => {
-    result.current.setNewActive(newActive);
+    const initActive = [
+      {
+        id: 0,
+        title: 'Todo List1',
+        tasks: [
+          { taskNum: 0, value: 'Task1', checked: false },
+          { taskNum: 1, value: 'Task2', checked: false },
+        ],
+      },
+      {
+        id: 1,
+        title: '',
+        tasks: [
+          { taskNum: 0, value: 'TaskA', checked: false },
+          { taskNum: 1, value: 'TaskB', checked: false },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Todo ListA',
+        tasks: [
+          { taskNum: 0, value: 'TaskAlpha', checked: false },
+          { taskNum: 1, value: 'TaskBeta', checked: false },
+        ],
+      },
+    ];
+
+    act(() => {
+      result.current.setNewActive(initActive);
+    });
+
+    act(() => {
+      result.current.delActive(1);
+    });
+
+    expect(result.current.active).toEqual([
+      {
+        id: 0,
+        title: 'Todo List1',
+        tasks: [
+          { taskNum: 0, value: 'Task1', checked: false },
+          { taskNum: 1, value: 'Task2', checked: false },
+        ],
+      },
+      {
+        id: 1,
+        title: 'Todo ListA',
+        tasks: [
+          { taskNum: 0, value: 'TaskAlpha', checked: false },
+          { taskNum: 1, value: 'TaskBeta', checked: false },
+        ],
+      },
+    ]);
   });
-  expect(result.current.active).toEqual(newActive);
-});
-
-test('DeleteActive Test', () => {
-  const { result } = renderHook(() => useActive(), {
-    wrapper: TestWrapper,
-  });
-
-  const initActive = [
-    {
-      id: 0,
-      title: 'Todo List1',
-      tasks: [
-        { taskNum: 0, value: 'Task1', checked: false },
-        { taskNum: 1, value: 'Task2', checked: false },
-      ],
-    },
-    {
-      id: 1,
-      title: '',
-      tasks: [
-        { taskNum: 0, value: 'TaskA', checked: false },
-        { taskNum: 1, value: 'TaskB', checked: false },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Todo ListA',
-      tasks: [
-        { taskNum: 0, value: 'TaskAlpha', checked: false },
-        { taskNum: 1, value: 'TaskBeta', checked: false },
-      ],
-    },
-  ];
-
-  act(() => {
-    result.current.setNewActive(initActive);
-  });
-
-  act(() => {
-    result.current.delActive(1);
-  });
-
-  expect(result.current.active).toEqual([
-    {
-      id: 0,
-      title: 'Todo List1',
-      tasks: [
-        { taskNum: 0, value: 'Task1', checked: false },
-        { taskNum: 1, value: 'Task2', checked: false },
-      ],
-    },
-    {
-      id: 1,
-      title: 'Todo ListA',
-      tasks: [
-        { taskNum: 0, value: 'TaskAlpha', checked: false },
-        { taskNum: 1, value: 'TaskBeta', checked: false },
-      ],
-    },
-  ]);
 });
--- src/hooks/__test__/useComp.test.tsx
+++ src/hooks/__test__/useComp.test.tsx
@@ -8,97 +8,101 @@ interface Props {
   children: ReactNode;
 }
 
-const initializeState = ({ set }: MutableSnapshot): void => {
-  set(compState, []);
-};
-const TestWrapper: FC<Props> = ({ children }) => <RecoilRoot initializeState={initializeState}>{children}</RecoilRoot>;
+describe('UseComp Test', () => {
+  const initializeState = ({ set }: MutableSnapshot): void => {
+    set(compState, []);
+  };
+  const TestWrapper: FC<Props> = ({ children }) => (
+    <RecoilRoot initializeState={initializeState}>{children}</RecoilRoot>
+  );
 
-test('SetNewComp Test', () => {
-  const { result } = renderHook(() => useComp(), {
-    wrapper: TestWrapper,
+  test('SetNewComp Test', () => {
+    const { result } = renderHook(() => useComp(), {
+      wrapper: TestWrapper,
+    });
+
+    const newComp = [
+      {
+        id: 0,
+        title: 'Todo List1',
+        tasks: [
+          { taskNum: 0, value: 'Task1', checked: false },
+          { taskNum: 1, value: 'Task2', checked: false },
+        ],
+      },
+      {
+        id: 1,
+        title: '',
+        tasks: [
+          { taskNum: 0, value: 'TaskA', checked: false },
+          { taskNum: 1, value: 'TaskB', checked: false },
+        ],
+      },
+    ];
+
+    act(() => {
+      result.current.setNewComp(newComp);
+    });
+    expect(result.current.comp).toEqual(newComp);
   });
 
-  const newComp = [
-    {
-      id: 0,
-      title: 'Todo List1',
-      tasks: [
-        { taskNum: 0, value: 'Task1', checked: false },
-        { taskNum: 1, value: 'Task2', checked: false },
-      ],
-    },
-    {
-      id: 1,
-      title: '',
-      tasks: [
-        { taskNum: 0, value: 'TaskA', checked: false },
-        { taskNum: 1, value: 'TaskB', checked: false },
-      ],
-    },
-  ];
+  test('DeleteComp Test', () => {
+    const { result } = renderHook(() => useComp(), {
+      wrapper: TestWrapper,
+    });
 
-  act(() => {
-    result.current.setNewComp(newComp);
+    const initComp = [
+      {
+        id: 0,
+        title: 'Todo List1',
+        tasks: [
+          { taskNum: 0, value: 'Task1', checked: false },
+          { taskNum: 1, value: 'Task2', checked: false },
+        ],
+      },
+      {
+        id: 1,
+        title: '',
+        tasks: [
+          { taskNum: 0, value: 'TaskA', checked: false },
+          { taskNum: 1, value: 'TaskB', checked: false },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Todo ListA',
+        tasks: [
+          { taskNum: 0, value: 'TaskAlpha', checked: false },
+          { taskNum: 1, value: 'TaskBeta', checked: false },
+        ],
+      },
+    ];
+
+    act(() => {
+      result.current.setNewComp(initComp);
+    });
+
+    act(() => {
+      result.current.delComp(1);
+    });
+
+    expect(result.current.comp).toEqual([
+      {
+        id: 0,
+        title: 'Todo List1',
+        tasks: [
+          { taskNum: 0, value: 'Task1', checked: false },
+          { taskNum: 1, value: 'Task2', checked: false },
+        ],
+      },
+      {
+        id: 1,
+        title: 'Todo ListA',
+        tasks: [
+          { taskNum: 0, value: 'TaskAlpha', checked: false },
+          { taskNum: 1, value: 'TaskBeta', checked: false },
+        ],
+      },
+    ]);
   });
-  expect(result.current.comp).toEqual(newComp);
-});
-
-test('DeleteComp Test', () => {
-  const { result } = renderHook(() => useComp(), {
-    wrapper: TestWrapper,
-  });
-
-  const initComp = [
-    {
-      id: 0,
-      title: 'Todo List1',
-      tasks: [
-        { taskNum: 0, value: 'Task1', checked: false },
-        { taskNum: 1, value: 'Task2', checked: false },
-      ],
-    },
-    {
-      id: 1,
-      title: '',
-      tasks: [
-        { taskNum: 0, value: 'TaskA', checked: false },
-        { taskNum: 1, value: 'TaskB', checked: false },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Todo ListA',
-      tasks: [
-        { taskNum: 0, value: 'TaskAlpha', checked: false },
-        { taskNum: 1, value: 'TaskBeta', checked: false },
-      ],
-    },
-  ];
-
-  act(() => {
-    result.current.setNewComp(initComp);
-  });
-
-  act(() => {
-    result.current.delComp(1);
-  });
-
-  expect(result.current.comp).toEqual([
-    {
-      id: 0,
-      title: 'Todo List1',
-      tasks: [
-        { taskNum: 0, value: 'Task1', checked: false },
-        { taskNum: 1, value: 'Task2', checked: false },
-      ],
-    },
-    {
-      id: 1,
-      title: 'Todo ListA',
-      tasks: [
-        { taskNum: 0, value: 'TaskAlpha', checked: false },
-        { taskNum: 1, value: 'TaskBeta', checked: false },
-      ],
-    },
-  ]);
 });
